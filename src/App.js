14462a3316fcb746562562945c2947780c27788e
Earthpasserby <iwuno68@gmail.com>
2024-05-13 12:25:20 +0100

    More commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  BrowserRouter,
+} from "react-router-dom";
+import HomeEvent from "./Components/HomeEvent";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <Routes>
+        <Route>
+          <Route index element={<HomeEvent />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/Components/HomeEvent.jsx
@@ -0,0 +1,88 @@
+import {
+  Button,
+  Container,
+  Row,
+  Navbar,
+  Nav,
+  Card,
+  Form,
+} from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { FcHome } from "react-icons/fc";
+const HomeEvent = () => {
+  return (
+    <>
+      <Container>
+        <Row>
+          <Navbar expand="lg">
+            <Container fluid>
+              <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
+              <Navbar.Toggle aria-controls="navbarScroll" />
+              <Navbar.Collapse id="navbarScroll">
+                <Nav
+                  variant="underline"
+                  className="me-auto my-2 my-lg-0"
+                  style={{ maxHeight: "100px" }}
+                  navbarScroll
+                >
+                  <Nav.Item>
+                    <Nav.Link href="/HomeEvent">Events</Nav.Link>
+                  </Nav.Item>
+                  <Nav.Item>
+                    <Nav.Link eventKey="link-1">Nearby Event</Nav.Link>
+                  </Nav.Item>
+                  <Nav.Item>
+                    <Nav.Link eventKey="link-2">Closed Event</Nav.Link>
+                  </Nav.Item>
+                </Nav>
+                <Form className="d-flex">
+                  <Form.Control
+                    type="search"
+                    placeholder="Search Events"
+                    className="me-2"
+                    aria-label="Search"
+                  />
+                  <Button variant="outline-success">Search</Button>
+                </Form>
+              </Navbar.Collapse>
+            </Container>
+          </Navbar>
+          <div></div>
+        </Row>
+      </Container>
+
+      <Container>
+        <Row>
+          <div>
+            <Card body>
+              <div>
+                <div class="d-flex justify-content-around">
+                  <div>
+                    <h3>Carnaval de São Paulo</h3>
+                    <p>Birigui, São Paulo, Brazil</p>
+                    <p>Fri, 9 Feb 2024-Wed, 14 Feb 2024-0300 (6-days)</p>
+                    <div>
+                      <p>
+                        <FcHome className="me-2" />
+                        FESTIVALS
+                      </p>
+                    </div>
+                  </div>
+                  <div>
+                    <pre>$480,005,100</pre>
+                    <pre>Predicted Event Spend (Usd) </pre>
+                  </div>
+                  <div>
+                    <pre>150,0000</pre>
+                    <pre>PHQ Attendance</pre>
+                  </div>
+                </div>
+              </div>
+            </Card>
+          </div>
+        </Row>
+      </Container>
+    </>
+  );
+};
+export default HomeEvent;
